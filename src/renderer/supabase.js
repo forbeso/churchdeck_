ec47420267fd29eeb57699ff8607be87582b1318
od <65794818+forbeso@users.noreply.github.com>
2023-06-27 14:17:31 -0400

    Update supabase.js

--- src/renderer/supabase.js
+++ src/renderer/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://loqlxhsgwifkynavahyc.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvcWx4aHNnd2lma3luYXZhaHljIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MjM1Mzg2OCwiZXhwIjoxOTk3OTI5ODY4fQ.Ylu8707EXojukkRHJ50jOyRqo7wgw6osWjvH40QL8p4';
+const supabaseUrl = '';
+const supabaseKey ='';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
